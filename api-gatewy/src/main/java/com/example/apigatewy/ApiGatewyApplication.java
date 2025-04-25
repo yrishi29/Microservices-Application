8159b1205742d62858c813d953105a7832c1ba98
rishi <rishikeshy612@gmail.com>
2025-04-25 14:11:13 +0530

    Built API Gateway

---
+++ api-gatewy/src/main/java/com/example/apigatewy/ApiGatewyApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatewy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewyApplication.class, args);
+    }
+
+}
---
+++ api-gatewy/src/test/java/com/example/apigatewy/ApiGatewyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigatewy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
