117fb2600ce082305d3c56c3b8d4aa1c1ff8c999
rishi <rishikeshy612@gmail.com>
2025-04-28 11:55:00 +0530

    Added Security using Keycloak

---
+++ api-gatewy/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
+        return serverHttpSecurity.build();
+    }
+
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder() {
+//        String issuerUri = "http://localhost:8080/realms/SpringBoot-Microservices";
+//        return NimbusReactiveJwtDecoder
+//                .withJwkSetUri(issuerUri + "/protocol/openid-connect/certs")
+//                .build();
+//    }
+}
